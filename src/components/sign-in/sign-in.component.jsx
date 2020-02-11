dcf889a917d29ce476a43b6fb1e33f3c16206c99
Igor <igor@herolo.co.il>
2020-02-12 00:11:09 +0200

    added sign-in auth

--- src/components/sign-in/sign-in.component.jsx
+++ src/components/sign-in/sign-in.component.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import FormInput from '../form-input/form-input.component';
 import CustomButton from './../custom-button/custom-button.component';
-import {signInWithGoogle} from '../../firebase/firebase.util';
+import {auth, signInWithGoogle} from '../../firebase/firebase.util';
 
 import '../sign-in/sign-in.styles.scss';
 
@@ -15,9 +15,16 @@ class SignIn extends React.Component {
      }
  }
 
- handleSubmit = event => {
+ handleSubmit = async event => {
      event.preventDefault();
-     this.setState({email: '', password: ''});
+     const {email, password} = this.state;
+
+     try {
+        await auth.signInWithEmailAndPassword(email, password);
+        this.setState({email: '', password: ''});
+     } catch(error) {
+        console.log(error);
+     }
  }
 
  handleChange = event => {
@@ -34,6 +41,7 @@ class SignIn extends React.Component {
              <form onSubmit={this.handleSubmit}>
                  <FormInput 
                     name="email"
+                    type="email"
                     value={this.state.email}
                     handleChange={this.handleChange}
                     label="email"
@@ -41,6 +49,7 @@ class SignIn extends React.Component {
                
                  <FormInput 
                     name="password"
+                    type="password"
                     value={this.state.password}
                     handleChange={this.handleChange}
                     label="password"
