a49ff5e00d36a15886e67aea76fd14572863bf95
Igor <igor@herolo.co.il>
2020-02-11 10:48:32 +0200

    added router

--- src/App.js
+++ src/App.js
@@ -2,12 +2,22 @@ import React from 'react';
 import './App.css';
 import HomePage from './pages/homepage/homePage.component';
 import './pages/homepage/homePage.styles.scss';
+import { Route, Switch } from 'react-router-dom';
+
+const HatsPage = () => (
+  <div>
+    <h1>Hats Page</h1>
+  </div>
+)
 
 
 function App() {
   return (
     <div className="App">
-        <HomePage/>
+      <Switch>
+        <Route exact path='/' component={HomePage} />
+        <Route path='/shop/hats' component={HatsPage} />
+      </Switch>
     </div>
   );
 }
