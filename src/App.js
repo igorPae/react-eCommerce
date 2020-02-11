36e657b710e5f78b66c88ea04c0a4e48fb2496d8
Igor <igor@herolo.co.il>
2020-02-12 00:05:30 +0200

    Added sign-up

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import HomePage from './pages/homepage/homePage.component';
 import ShopPage from './pages/shop/shop.component';
 import SignInAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component';
 import Header from './components/header/header.component';
-import {auth} from './firebase/firebase.util';
+import {auth, createUserProfileDocument} from './firebase/firebase.util';
 
 import './pages/homepage/homePage.styles.scss';
 import { Route, Switch } from 'react-router-dom';
@@ -27,10 +27,21 @@ class App extends React.Component {
   unsubscribeFromAuth = null;
 
   componentDidMount() {
-    this.unsubscribeFromAuth = auth.onAuthStateChanged(user => {
-      this.setState({currentUser: user});
+    this.unsubscribeFromAuth = auth.onAuthStateChanged(async userAuth => {
+      if (userAuth) {
+        const userRef = await createUserProfileDocument(userAuth);
 
-      console.log(user);
+        userRef.onSnapshot(snapShot => {
+          this.setState({
+            currentUser: {
+              id: snapShot.id,
+              ...snapShot.data()
+            }
+          }, () => console.log(this.state))
+        })
+      } else {
+        this.setState({currentUser: userAuth});
+      }
     })
   }
 
