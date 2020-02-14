eb7ca257532475eaf984d0610d84a863ee8b264e
Igor <igor@herolo.co.il>
2020-02-14 17:39:53 +0200

    Added collection to redux

--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -1,19 +1,10 @@
 import React from 'react';
-import SHOP_DATA from './shop.data';
-import CollectionPreview from '../../components/preview-collection/collection-preview'
+import CollectionPreview from '../../components/preview-collection/collection-preview';
+import { connect } from 'react-redux';
+import { createStructuredSelector } from 'reselect';
+import { selectCollections } from '../../redux/shop/shop.selectors';
 
-class ShopPage extends React.Component {
-    constructor(props) {
-        super(props);
-
-        this.state = {
-            collections: SHOP_DATA
-        }
-    }
-
-    render() {
-        const {collections} = this.state;
-        return (
+const ShopPage = ({collections}) => (
             <div className='shop-page'>
                 {
                     collections.map(({id, ...otherProps}) => (
@@ -21,8 +12,10 @@ class ShopPage extends React.Component {
                     ))
                 }
             </div>
-        )
-    }
-}
+        );
 
-export default ShopPage;
+const mapStateToProps = createStructuredSelector({
+    collections: selectCollections
+})
+
+export default connect(mapStateToProps)(ShopPage) ;
