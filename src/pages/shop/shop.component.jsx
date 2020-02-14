0a5ffc1938f6a3b0c84a09e35746b37ebfe544b2
Igor <igor@herolo.co.il>
2020-02-14 19:48:50 +0200

    Change data structure

--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -1,21 +1,14 @@
 import React from 'react';
-import CollectionPreview from '../../components/preview-collection/collection-preview';
-import { connect } from 'react-redux';
-import { createStructuredSelector } from 'reselect';
-import { selectCollections } from '../../redux/shop/shop.selectors';
+import CollectionOverview from '../../components/collection-overview/collection-overview.component';
+import { Route } from 'react-router-dom';
+import CollectionPage from '../collection/collection.component';
 
-const ShopPage = ({collections}) => (
+
+const ShopPage = ({match}) => (
             <div className='shop-page'>
-                {
-                    collections.map(({id, ...otherProps}) => (
-                        <CollectionPreview key={id} {...otherProps} />
-                    ))
-                }
+                 <Route exact path={`${match.path}`} component={CollectionOverview} />
+                 <Route path={`${match.path}/:collectionId`} component={CollectionPage} />
             </div>
         );
 
-const mapStateToProps = createStructuredSelector({
-    collections: selectCollections
-})
-
-export default connect(mapStateToProps)(ShopPage) ;
+export default ShopPage;
