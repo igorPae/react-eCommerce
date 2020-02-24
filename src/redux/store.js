01c412805b7a83d6e1d61ad13b4373833469edb3
Igor <igor@herolo.co.il>
2020-02-24 08:46:00 +0200

    Add saga

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,13 +1,19 @@
 import { createStore, applyMiddleware } from 'redux';
 import { persistStore } from 'redux-persist';
 import logger from 'redux-logger';
-import thunk from 'redux-thunk';
+import createSagaMiddleware from 'redux-saga';
+
+import { fetchCollectionStart } from './shop/shop.saga';
 
 import rootReducer from './root-reducer';
 
-const middlewares = [thunk];
+const sagaMiddlware = createSagaMiddleware();
+
+const middlewares = [sagaMiddlware, logger];
 
 export const store = createStore(rootReducer, applyMiddleware(...middlewares));
+
+sagaMiddlware.run(fetchCollectionStart);
 
 export const persistor = persistStore(store);
 
